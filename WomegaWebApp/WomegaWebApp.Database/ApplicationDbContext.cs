e70c4bfb17322b09555103b8b98c60b03fbe0eca
Prithvipatel007 <prithvipatel007@gmail.com>
2021-03-20 13:36:02 +0100

    ApplicationDbContext.cs file for database implemented

---
+++ WomegaWebApp/WomegaWebApp.Database/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WomegaWebApp.Database
+{
+    class ApplicationDbContext
+    {
+    }
+}
--- WomegaWebApp/WomegaWebApp.Database/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WomegaWebApp.Database
-{
-    public class Class1
-    {
-    }
-}
