7a988ebbf7addd309b7ca348b9990e9db67830e0
Prithvipatel007 <prithvipatel007@gmail.com>
2021-03-20 13:03:20 +0100

    Base Project is created

---
+++ WomegaWebApp/WomegaWebApp.Application/Information.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WomegaWebApp.Application
+{
+    /*
+     This Project is responsible for handling our domain which means here, we are gonna create out products,
+    storing them into database and it's basically a pipeline between domain and UI and database
+     */
+
+    public class Information
+    {
+    }
+}
--- WomegaWebApp/WomegaWebApp.UI/Program.cs
+++ WomegaWebApp/WomegaWebApp.UI/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WomegaWebApp
+namespace WomegaWebApp.UI
 {
     public class Program
     {
