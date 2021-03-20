7b15d7bc90b5d92ed92da92689d57a0b3200a4b8
Prithvipatel007 <prithvipatel007@gmail.com>
2021-03-20 21:31:31 +0100

    Product fields moved to the Shop.Application for the MVVM capability

--- Shop/Shop.UI/Pages/Index.cshtml.cs
+++ Shop/Shop.UI/Pages/Index.cshtml.cs
@@ -17,13 +17,6 @@ namespace Shop.UI.Pages
         [BindProperty]
         public ProductViewModel Product { get; set; }
 
-        public class ProductViewModel
-        {
-            public string Name { get; set; }
-            public string Description { get; set; }
-            public decimal Value { get; set; }
-        }
-
         public IndexModel(ApplicationDbContext ctx)
         {
             _ctx = ctx;
@@ -36,7 +29,7 @@ namespace Shop.UI.Pages
 
         public async Task<IActionResult> OnPost()
         {
-            await new CreateProduct(_ctx).Do(Product.Name, Product.Description, Product.Value);
+            await new CreateProduct(_ctx).Do(Product);
 
             return RedirectToPage("Index");
         }
