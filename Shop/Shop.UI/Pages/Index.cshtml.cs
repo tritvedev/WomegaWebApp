59f2da9f7726b5a7edb4e1c278ea52380627e776
Prithvipatel007 <prithvipatel007@gmail.com>
2021-03-20 20:36:32 +0100

    Add product to database feature implemented

--- Shop/Shop.UI/Pages/Index.cshtml.cs
+++ Shop/Shop.UI/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Shop.Application.Products;
+using Shop.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +12,33 @@ namespace Shop.UI.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private ApplicationDbContext _ctx;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [BindProperty]
+        public ProductViewModel Product { get; set; }
+
+        public class ProductViewModel
         {
-            _logger = logger;
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
+        }
+
+        public IndexModel(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
         }
 
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await new CreateProduct(_ctx).Do(Product.Name, Product.Description, Product.Value);
+
+            return RedirectToPage("Index");
         }
     }
 }
