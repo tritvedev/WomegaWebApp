90fd8cb686a643e98c1a5270ec19b752bd7baeb2
Prithvipatel007 <prithvipatel007@gmail.com>
2021-04-01 15:40:15 +0200

    basic Vue toggling example implemented

--- Shop/Shop.UI/wwwroot/js/admin/main.js
+++ Shop/Shop.UI/wwwroot/js/admin/main.js
@@ -2,6 +2,19 @@
     el: '#app',
     data: {
         price: 0,
-
+        showPrice: true,
+    },
+    methods: {
+        togglePrice: function () {
+            this.showPrice = !this.showPrice;
+        },
+        alert(v) {
+            alert(v)
+        }
+    },
+    computed: {
+        formatPrice: function () {
+            return "€ " + this.price;
+        }
     }
 });
