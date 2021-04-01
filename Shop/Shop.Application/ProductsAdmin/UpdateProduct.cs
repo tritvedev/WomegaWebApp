0bf9450320c027d6c623226aaec4e1b0ea38b7ad
Prithvipatel007 <prithvipatel007@gmail.com>
2021-04-01 23:04:48 +0200

    Update and Delete Product functionality implemented

--- Shop/Shop.Application/ProductsAdmin/UpdateProduct.cs
+++ Shop/Shop.Application/ProductsAdmin/UpdateProduct.cs
@@ -2,6 +2,7 @@
 using Shop.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,13 +19,26 @@ namespace Shop.Application.ProductsAdmin
 
         public async Task<Response> Do(Request request)
         {
+            var product = _context.Products.FirstOrDefault(x => x.Id == request.Id);
+
+            product.Name = request.Name;
+            product.Description = request.Description;
+            product.Value = request.Value;
+
             await _context.SaveChangesAsync();
 
-            return new Response();
+            return new Response
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Value = product.Value
+            };
         }
 
         public class Request
         {
+            public int Id { get; set; }
             public string Name { get; set; }
             public string Description { get; set; }
             public decimal Value { get; set; }
--- Shop/Shop.UI/wwwroot/js/admin/main.js
+++ Shop/Shop.UI/wwwroot/js/admin/main.js
@@ -2,14 +2,39 @@
     el: '#app',
     data: {
         loading: false,
+        objectIndex: 0,
         productModel: {
+            id: 0,
             name: "Product Name",
             description: "Product Description",
             value: 1.99
         },
         products: []
     },
+    mounted() {
+        this.getProducts()
+    },
     methods: {
+        getProduct(id) {
+            this.loading = true;
+            axios.get('/Admin/products/' + id)
+                .then(res => {
+                    console.log(res);
+                    var product = res.data;
+                    this.productModel = {
+                        id: product.id,
+                        name: product.name,
+                        description: product.description,
+                        value: product.value
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                })
+        },
         getProducts() {
             this.loading = true;
             axios.get('/Admin/products')
@@ -37,6 +62,38 @@
                 .then(() => {
                     this.loading = false;
                 })
+        },
+        updateProduct() {
+            this.loading = true;
+            axios.put('/Admin/products', this.productModel)
+                .then(res => {
+                    console.log(res.data);
+                    this.products.splice(this.objectIndex, 1, res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                })
+        },
+        deleteProduct(id, index) {
+            this.loading = true;
+            axios.delete('/Admin/products/' + id)
+                .then(res => {
+                    console.log(res);
+                    this.products.splice(index, 1)
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                })
+        },
+        editProduct(id, index) {
+            this.objectIndex = index;
+            this.getProduct(id)
         }
     },
     computed: {
