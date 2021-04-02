83469e30c4a6edea1ee0495bac6ed1a6918ac6f8
Prithvipatel007 <prithvipatel007@gmail.com>
2021-04-02 18:26:17 +0200

    product view for customer basic functionality implemented

---
+++ Shop/Shop.UI/Pages/Product.cshtml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shop.Application.Products;
+using Shop.Database;
+
+namespace Shop.UI.Pages
+{
+    public class ProductModel : PageModel
+    {
+        private ApplicationDbContext _ctx;
+
+        public ProductModel(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public GetProduct.ProductViewModel Product { get; set; }
+
+
+
+        public IActionResult OnGet(string name)
+        {
+            Product = new GetProduct(_ctx).Do(name.Replace("-"," "));
+            if(Product == null)
+            {
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
