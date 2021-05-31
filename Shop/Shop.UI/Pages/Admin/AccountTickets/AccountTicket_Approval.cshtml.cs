4a18bfce2a6bd29bcac940c98c821b9a4b944b9e
Umang Kaswala <kaswalaumangb@gmail.com>
2021-05-31 15:58:47 +0200

    adding accoung tickets

---
+++ Shop/Shop.UI/Pages/Admin/AccountTickets/AccountTicket_Approval.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shop.UI.Pages.Admin.AccountTickets
+{
+    public class AccountTicket_ApprovalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
